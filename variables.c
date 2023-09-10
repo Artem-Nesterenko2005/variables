c93691b59f216a875e975a24c462c154ab309c00
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2023-09-10 16:28:23 +0500

    Add files via upload

---
+++ variables.c
@@ -0,0 +1,19 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int variable1 = 0;
+	int	variable2 = 0;
+	printf("Enter the first integer variable ");
+	scanf_s("%d", &variable1);
+	printf("Enter the second integer variable ");
+	scanf_s("%d", &variable2);
+	variable1 = variable1 + variable2;
+	variable2 = variable1 - variable2;
+	variable1 = variable1 - variable2;
+	printf("First variable = %d\n", variable1);
+	printf("Second variable = %d", variable2);
+}
+
+
