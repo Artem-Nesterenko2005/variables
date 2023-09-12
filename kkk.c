6e8d0848dfdca45dfab1a7c20e3cee2205ba5e4e
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2023-09-13 00:14:50 +0500

    Add files via upload

---
+++ kkk.c
@@ -0,0 +1,31 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+int main()
+{
+	int divisible = 0;
+	int	divider = 0;
+	printf("Enter divisible ");
+	scanf_s("%d", &divisible);
+	printf("Enter divider ");
+	scanf_s("%d", &divider);
+	int sign = 1;
+	if (divisible * divider < 0)
+		sign = -1;
+	divisible = abs(divisible);
+	divider = abs(divider);
+	if (divisible > divider)
+	{
+		int counter = 0;
+		while (divisible > divider)
+		{
+			divisible = divisible - divider;
+			counter += 1;
+		}
+		int incompleteQuotient = counter;
+		printf("%d", sign * incompleteQuotient);
+	}
+	else
+	{
+		printf("0");
+	}
+}
